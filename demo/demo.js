868be9bafe2db217fb0efba6accb420d8a6ca3e4
seregpie@gmail.com <seregpie@gmail.com>
2017-09-18 22:08:34 +0200

    17.9.18

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,9 @@
 (function() {
 
+	let _isNil = function(value) {
+		return value === null || value === undefined;
+	};
+
 	new Vue({
 		el: '#a',
 
@@ -23,27 +27,52 @@
 					key() {
 						return `app/${this.storedNumber}/string`;
 					},
-					value: undefined,
+					type: {
+						parse(value) {
+							return value;
+						},
+						stringify(value) {
+							return value;
+						},
+					},
 					default() {
 						return 'dummy';
 					},
+					storage: localStorage,
+
 				};
+				Vue.set(ooo, 'dummy', Date.now());
+
+				window.addEventListener('storage', function(event) {
+					console.log(event);
+					ooo.dummy = Date.now();
+				}, false);
+
 
 				return {
 					get() {
+						let dummy = ooo.dummy;
+
 						let key = ooo.key.call(this);
-						let value = localStorage[key];
-						console.log(value);
-						if (value === undefined) {
+						let value =  ooo.storage.getItem(key);
+						console.log('get', value);
+						if (_isNil(value)) {
 							value = ooo.default.call(this);
+						} else {
+							value = ooo.type.parse.call(this, value);
 						}
 						return value;
 					},
 
 					set(value) {
+						console.log('set', value);
 						let key = ooo.key.call(this);
-						localStorage[key] = value;
-
+						if (_isNil(value)) {
+							ooo.storage.removeItem(key);
+						} else {
+							value = ooo.type.stringify.call(this, value);
+							ooo.storage.setItem(key, value);
+						}
 					},
 				};
 			})(),
