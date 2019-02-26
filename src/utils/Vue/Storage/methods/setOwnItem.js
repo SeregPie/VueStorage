1e263b62948464128166ffa403ba2e5a00d6bdf8
Sergej Sintschilin <seregpie@gmail.com>
2019-02-26 16:20:25 +0100

    fix when delete item

--- src/utils/Vue/Storage/methods/setOwnItem.js
+++ src/utils/Vue/Storage/methods/setOwnItem.js
@@ -2,9 +2,8 @@ import Lang_isNil from '../../../Lang/isNil';
 
 export default function(key, value) {
 	let {items} = this;
+	this.$set(items, key, value);
 	if (Lang_isNil(value)) {
 		this.$delete(items, key);
-	} else {
-		this.$set(items, key, value);
 	}
 }
